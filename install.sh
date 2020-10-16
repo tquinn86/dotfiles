6de09e1e4adc0f991375e721003ecd385c3cb461
TQ <tquinn86@github.com>
2020-10-16 19:38:48 -0400

    aliases

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 git config --global user.name "TQ"
 git config --global user.email "tquinn86@github.com"
 git config --global push.default simple
-git config --global alias.cob "!f() { git checkout -b $USER/$1; }; f;"
 
 # use bash
 ln -s ~/.dotfiles/.bash_profile ~/
+ln -s ~/.dotfiles/git/gitconfig.aliases.symlink ~/github.aliases
+git config --global include.path ~/.gitconfig.aliases
 bash
 
