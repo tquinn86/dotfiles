6c1c903e4e74743bb81fc4b2d00c22ff4a7a006c
TQ <tquinn86@github.com>
2020-10-16 11:43:14 -0400

    Update install.sh
    git alias for checkout -b

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 git config --global user.name "TQ"
 git config --global user.email "tquinn86@github.com"
-git config --globla push.default simple
+git config --global push.default simple
+git config --global alias.cob "!f() { git checkout -b $USER/$1; }; f;"
 
 # use bash
 ln -s ~/.dotfiles/.bash_profile ~/
